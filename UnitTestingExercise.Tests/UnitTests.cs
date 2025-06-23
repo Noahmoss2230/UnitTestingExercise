cd405699311fe75656b5eb82a598beda4c1ba5f2
Noah Moss <noahmoss50@gmail.com>
2025-06-23 14:28:25 -0500

    Added more InLineData

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -8,6 +8,10 @@ namespace UnitTestingExercise.Tests
         [Theory]
         [InlineData(1, 2, 3, 6)]
         [InlineData(5,5,5,15)]
+        [InlineData(10,10,10,30)]
+        [InlineData(15,15,15,45)]
+        [InlineData(4,7,9,20)]
+        [InlineData(10,15,3,28)]
         public void AddTest(int num1, int num2, int num3, int expected)
         {
             //Start Step 3 here:
@@ -27,7 +31,12 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(5,5,0)]//Add test data <-------
+        [InlineData(5,5,0)]
+        [InlineData(25,5,20)]
+        [InlineData(30,10,20)]
+        [InlineData(35,15,20)]
+        [InlineData(40,20,20)]
+        //Add test data <-------
         public void SubtractTest(int minuend, int subtrhend, int expected)
         {
             //Start Step 5 here:
@@ -45,7 +54,11 @@ namespace UnitTestingExercise.Tests
 
         [Theory]
         [InlineData(2,2,4)]
-        [InlineData(4,5,20)]//Add test data <-------
+        [InlineData(4,5,20)]
+        [InlineData(5,5,25)]
+        [InlineData(5,10,50)]
+        [InlineData(3,15,45)]
+        //Add test data <-------
         public void MultiplyTest(int num1, int num2, int expected)
         {
             //Start Step 7 here:
@@ -62,7 +75,12 @@ namespace UnitTestingExercise.Tests
         }
 
         [Theory]
-        [InlineData(4,2,2)]//Add test data <-------
+        [InlineData(4,2,2)]
+        [InlineData(8,2,4)]
+        [InlineData(16,4,4)]
+        [InlineData(32,8,4)]
+        [InlineData(12,3,4)]
+        //Add test data <-------
         public void DivideTest(int num1, int num2, int expected)
         {
             //Arrange
